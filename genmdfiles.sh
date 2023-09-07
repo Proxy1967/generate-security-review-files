fe70c3404aa6a65aca99d6fade29bbf5fcf38cca
Filip Kirin <72068235+Proxy1967@users.noreply.github.com>
2023-09-07 04:26:12 +0200

    Add the bash script

---
+++ genmdfiles.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+echo -e "# Contract Name\n
+## State variables\n
+## External / public functions\n
+### functionName()\n
+-
+-
+- Q:" > Notes.md
+
+echo -e "# High\n
+Sherlock:
+## [H-01]
+## Summary
+## Vulnerability Detail
+## Impact
+## Code Snippet
+## Tool used
+## Recommendation\n
+Code4rena:
+## [H-01]
+## Impact
+Detailed description of the impact of this finding.
+## Proof of Concept
+Provide direct links to all referenced code in GitHub. Add screenshots, logs, or any other relevant proof that illustrates the concept.
+## Tools Used
+## Recommended Mitigation Steps
+
+# Med\n
+Sherlock:
+## [M-01]
+## Summary
+## Vulnerability Detail
+## Impact
+## Code Snippet
+## Tool used
+## Recommendation\n
+Code4rena:
+## [M-01]
+## Impact
+Detailed description of the impact of this finding.
+## Proof of Concept
+Provide direct links to all referenced code in GitHub. Add screenshots, logs, or any other relevant proof that illustrates the concept.
+## Tools Used
+## Recommended Mitigation Steps
+
+# Low and Non-Critical Template\n
+### [Low Risk](#low-risk-1)\n
+| Total Low Risk Issues | X |
+|:--:|:--:|\n
+| Count | Title | Instances |
+|:--:|:-------| :--: |
+| [L-01](#) | XXX | X |
+| [L-02](#) | XXX | X |
+| [L-03](#) | XXX | X |\n
+### [Non-Critical](#non-critical-1)\n
+| Total Non-Critical Issues | X |
+|:--:|:--:|\n
+| Count | Title | Instances |
+|:--:|:-------| :--: |
+| [NC-01](#) | XXX | X |
+| [NC-02](#) | XXX | X |
+| [NC-03](#) | XXX | X |\n
+## Low Risk\n
+### [L-01] XXX\n
+### [L-02] XXX\n
+### [L-03] XXX\n
+## Non-critical\n
+### [NC-01] XXX\n
+### [NC-02] XXX\n
+### [NC-03] XXX
+
+# GasOpt" > Issues.md
